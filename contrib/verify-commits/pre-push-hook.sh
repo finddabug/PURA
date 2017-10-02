8bf4ef50c3936449f0e6261bacb6d66544c29a76
finddabug <1bier4me2@gmail.com>
2017-10-03 00:27:25 +0200

    No purapay directory

--- contrib/verify-commits/pre-push-hook.sh
+++ contrib/verify-commits/pre-push-hook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! [[ "$2" =~ ^(git@)?(www.)?github.com(:|/)purapay/pura(.git)?$ ]]; then
+if ! [[ "$2" =~ ^(git@)?(www.)?github.com(:|/)puracore/pura(.git)?$ ]]; then
     exit 0
 fi
 
